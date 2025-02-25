593bcc2e3c5e55e73f12ed024839582e3d414dd1
maxirosanda <maxi_rosanda@hotmail.com>
2025-02-24 22:30:32 -0300

    clase 8

--- RecursosBackend-Adoptme-main/src/app.js
+++ RecursosBackend-Adoptme-main/src/app.js
@@ -9,16 +9,23 @@ import sessionsRouter from './routes/sessions.router.js';
 import { faker } from '@faker-js/faker';
 import errorHandler from './middlewares/errors.js'
 import { addLogger, logger } from './utils/logger.js';
+import { swaggerOptions } from './utils/swagger.js';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors'
 
 
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT||3000;
 const connection = mongoose.connect(process.env.MONGO_URL)
-
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(addLogger)
+const specs = swaggerJsdoc(swaggerOptions)
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(specs))
+
 app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
